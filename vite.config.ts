eb17f47cf8f8028f56f07bec1a8bb5062d6017d8
kh <kei.hirasawa@fourier.jp>
2025-01-02 13:24:20 +0900

    ビルドされたものをdocsフォルダに生成するように修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/test-site/' : '/',
   plugins: [react()],
+  build: {
+    outDir: 'docs', // ビルド成果物を'./docs'フォルダに出力
+  },
 });
