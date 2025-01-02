03567227aeeb0740443908492f210155a0dd2d1f
kh <kei.hirasawa@fourier.jp>
2025-01-02 15:49:40 +0900

    Add build files to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ export default defineConfig({
   // // base: process.env.NODE_ENV === 'production' ? '/test-site/' : '/', // ローカルでは'/'を使用
   // base: '/', // リポジトリ名を指定
   plugins: [react()],
+
+  build:{
+    outDir: "docs", // 出力先を指定
+  }
 })
