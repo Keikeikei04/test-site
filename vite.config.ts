18a60fadf1a6a1acf40886ba18ac4c26cb28438c
kh <kei.hirasawa@fourier.jp>
2025-01-02 15:58:21 +0900

    パスを開発環境と本番環境で変化するように修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, ConfigEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  // // base: process.env.NODE_ENV === 'production' ? '/test-site/' : '/', // ローカルでは'/'を使用
-  // base: '/', // リポジトリ名を指定
+export default defineConfig(({ mode }: ConfigEnv) => ({
   plugins: [react()],
-
-  build:{
-    outDir: "docs", // 出力先を指定
-  }
-})
+  base: mode === 'production' ? '/test-site/' : '/', // 本番環境では /test-site/ を使用
+  build: {
+    outDir: 'docs', // 出力先を指定
+  },
+}));
