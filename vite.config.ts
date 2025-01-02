b4cc912c6f62299499e259c4d8cc0ccc32725930
kh <kei.hirasawa@fourier.jp>
2025-01-02 13:29:40 +0900

    index.htmlの参照先のパスをgithub pages用に修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/test-site/' : '/',
+  base: '/test-site/', // GitHub Pages用のベースパス
   plugins: [react()],
   build: {
     outDir: 'docs', // ビルド成果物を'./docs'フォルダに出力
